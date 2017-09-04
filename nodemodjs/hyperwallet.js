9bc370d9c733a558f2ef5487f4fa29579c1853fb
Unknown <calebcheongdnt@gmail.com>
2017-09-04 14:00:54 +0800

    getWalletbyID, insert new Wallet,

--- nodemodjs/hyperwallet.js
+++ nodemodjs/hyperwallet.js
@@ -4,50 +4,118 @@ var request = require('request');
 
 var url = "http://localhost:3000/"
 
-getWalletByID(123)
+// getWalletByID(12345);
+// insertNewClientWallet(123,123,0)
+// getClientWalletByClientID(12345);
 
-function getWalletByID(id) {
-    request(url + 'api/Wallet?filter=%7B%22clientID%22%3A%20%22' + id + '%22%7D', function (error, response, body) {
+// processTransaction(12345,123,10);
+
+function processTransaction(clientID,merchantID,amount){
+    var vpromise = getClientWalletByClientID(clientID)
+    vpromise.then((value)=>{
+        console.log("HERE"+value.value);
+        if(value.value-amount >= 0){
+            createTransaction(clientID,merchantID,amount)
+            console.log("Transaction Sucess!")
+        }
+        else{
+            console.log("insufficent funds please top up!")
+        }
+    })
+}
+
+// getClientWalletByClientID(123);
+
+
+function getClientWalletByClientID(id) {
+    return new Promise((resolve, reject) => {
+    request(url + 'api/clientWallet/clientWalletID%3A'+id, function (error, response, body) {
         if (error) {
             console.log('error:', error); // Print the error if one occurred 
             return;
         }
-        console.log('body:', body); 
-      });
-}
+        console.log('body:', body);
+        // console.log(JSON.parse(body)[0].clientID);
+        resolve(JSON.parse(body));
+    });
+    })
+};
 
-function insertNewWallet(id, clientid, value) {
-    request.post(url + 'api/wallet', 
-        {form:
+
+function insertNewClientWallet(id, clientid, value) {
+    request.post(url + 'api/clientWallet',
+        {
+            form:
             {
-                "$class": "org.acme.jenetwork.Wallet",
-                "walletID": "walletID:" + id,
+                "$class": "org.acme.jenetwork.clientWallet",
+                "clientWalletID": "clientWalletID:" + id,
                 "clientID": clientid,
                 "value": value
             }
         }, function (error, response, body) {
+            if (error) {
+                console.log('error:', error); // Print the error if one occurred 
+                return;
+            }
+            console.log('body:', body);
+        });
+}
+
+function getMerchantWalletByMerchantID(id) {
+    request(url + 'api/merchantWallet/merchantWalletID%3A' + id, function (error, response, body) {
         if (error) {
             console.log('error:', error); // Print the error if one occurred 
             return;
         }
-        console.log('body:', body); 
+        console.log('body:', body);
     });
 }
-// createTransaction(12345,2)
-function createTransaction(walletid, value) {
-    request.post(url + 'api/walletTransaction', 
-    {form:
+
+function insertNewMerchantWallet(id, merchantid, value) {
+    request.post(url + 'api/merchantWallet',
         {
-            "$class": "org.acme.jenetwork.walletTransaction",
-            "asset": "resource:org.acme.jenetwork.Wallet#walletID:" + walletid,
-            "newValue": value
-          
+            form:
+            {
+                "$class": "org.acme.jenetwork.merchantWallet",
+                "merchantWalletID": "merchantWalletID:" + id,
+                "merchantID": +merchantid,
+                "value": +value
+            }
+        }, function (error, response, body) {
+            if (error) {
+                console.log('error:', error); // Print the error if one occurred 
+                return;
+            }
+            console.log('body:', body);
+        });
+}
+
+function createTransaction(clientWalletID, merchantWalletID, value) {
+    request.post(url + 'api/walletTransactionPay',
+        {
+            form:
+            {
+                "$class": "org.acme.jenetwork.walletTransactionPay",
+                "asset": "resource:org.acme.jenetwork.clientWallet#clientWalletID:" + clientWalletID,
+                "asset2": "resource:org.acme.jenetwork.merchantWallet#merchantWalletID:" + merchantWalletID,
+                "amount": value
+
+            }
+        }, function (error, response, body) {
+            if (error) {
+                console.log('error:', error); // Print the error if one occurred 
+                return;
+            }
+            console.log('body:', body);
+        });
+}
+retrieveTransaction();
+function retrieveTransaction() {
+    request(url + 'api/system/transactions', function (error, response, body) {
+        if (error) {
+            console.log('error:', error); // Print the error if one occurred 
+            return;
         }
-    }, function (error, response, body) {
-    if (error) {
-        console.log('error:', error); // Print the error if one occurred 
-        return;
-    }
-    console.log('body:', body);
-});
+        console.log('body:', body);
+    });
 }
